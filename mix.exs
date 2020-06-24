46cac1ff63cd4b3d8e2e2dedeafd68ab5e4cc85c
Miguel Palhas <mpalhas@gmail.com>
2020-06-25 00:46:20 +0100

    Fixing elixir 1.8 support

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Fsmx.MixProject do
     [
       app: :fsmx,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       applications: applications(Mix.env()),
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
