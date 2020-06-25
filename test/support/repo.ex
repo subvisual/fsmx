6e85afb18e72f5274713e881ce80b918c0e0462e
Miguel Palhas <mpalhas@gmail.com>
2020-06-25 11:10:45 +0100

    Fixing migrations directory

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,5 +1,6 @@
 defmodule Fsmx.Repo do
   use Ecto.Repo,
-    otp_app: :postgrex,
-    adapter: Ecto.Adapters.Postgres
+    otp_app: :fsmx,
+    adapter: Ecto.Adapters.Postgres,
+    priv: "test/support/repo/migrations"
 end
