169115af421f0d2eeee888c0532b8e9f838155b4
Miguel Palhas <mpalhas@gmail.com>
2020-06-25 11:27:21 +0100

    code review

--- test/support/repo.ex
+++ test/support/repo.ex
@@ -1,5 +1,6 @@
 defmodule Fsmx.Repo do
   use Ecto.Repo,
     otp_app: :fsmx,
-    adapter: Ecto.Adapters.Postgres
+    adapter: Ecto.Adapters.Postgres,
+    show_sensitive_data_on_connection_error: true
 end
